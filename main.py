8fb4782a03c72c42c1ba4bf410ed9c68ae5b2335
arkadiusz-l <104087320+arkadiusz-l@users.noreply.github.com>
2024-04-08 14:27:01 +0200

    Add download images to user's Download directory

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import urllib.request
 from datetime import datetime
 import logging
-from os import path
+from os import path, environ
 import sys
 
 
@@ -27,7 +27,7 @@ def get_image_url_from_api(api: str, date: str, time: str) -> str:
     return url
 
 
-def download_image(url: str, path: str):
+def download_image(url: str, path: str) -> None:
     urllib.request.urlretrieve(url, path)
     print(f"The image has been saved in {path}.")
 
@@ -36,9 +36,11 @@ class HoursError(Exception):
     pass
 
 
-def get_filepath(date: str, time: str):
+def get_filepath(date: str, time: str) -> str:
+    download_dir = path.join(environ.get('HOMEPATH'), 'Downloads')
     filename = f"{date}T0{time}L.jpg" if len(str(time)) < 2 else f"{date}T{time}L.jpg"
-    filepath = path.join(path.dirname(__file__), filename)
+    # filepath = path.join(path.dirname(__file__), filename)
+    filepath = path.join(path.dirname(download_dir), filename)
     logging.debug(f"{filepath=}")
     return filepath
 
@@ -68,6 +70,6 @@ if __name__ == "__main__":
             print("Endpoint not found!")
         except KeyboardInterrupt:
             sys.exit("Program stopped by user.")
-        except urllib.error.URLError:
-            sys.exit("API not found! Check API URL or network connection.")
+        # except urllib.error.URLError:
+        #     sys.exit("API not found! Check API URL or network connection.")
     print("Done.")
