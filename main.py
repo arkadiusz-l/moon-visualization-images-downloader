9b72206d2df47801786346752f8a8d38be5f73a2
arkadiusz-l <104087320+arkadiusz-l@users.noreply.github.com>
2024-04-10 12:38:26 +0200

    Add getting date from user, add date validation

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 import logging
+import re
 import os
 import sys
 import requests
 
 
-def get_current_date_in_utc() -> str:
-    return str(datetime.now().date())
+def get_date_in_utc(date: str) -> str:
+    if date == "t":
+        return str(datetime.now().date())
+    elif date == "tm":
+        return str(datetime.now().date() + timedelta(days=1))
+    elif bool(datetime.strptime(date, "%Y-%m-%d")):
+        return date
+    else:
+        raise DateError
 
 
 def get_image_url_from_api(api: str, date: str, time: str) -> str:
@@ -36,6 +44,10 @@ class HoursError(Exception):
     pass
 
 
+class DateError(Exception):
+    pass
+
+
 def get_filepath(download_dir: str, date: str, time: str) -> str:
     filename = f"{date}T0{time}L.jpg" if len(time) < 2 else f"{date}T{time}L.jpg"
     filepath = os.path.join(download_dir, filename)
@@ -48,19 +60,20 @@ if __name__ == "__main__":
     logging.getLogger("urllib3").setLevel(logging.WARNING)  # disable standard DEBUG logs
     while True:
         try:
-            start = int(input("The hour of the first Moon visualization image (00-23): "))
-            end = int(input("The hour of the last Moon visualization image (00-23): "))
-            if start > end:
+            date = input("Enter the date for which I should download images (YYYY-MM-DD or 't' for Today, 'tm' for Tommorow): ")
+            start_hour = int(input("The hour of the first Moon visualization image (00-23): "))
+            end_hour = int(input("The hour of the last Moon visualization image (00-23): "))
+            if start_hour > end_hour:
                 raise HoursError
 
-            today = get_current_date_in_utc()
+            date = get_date_in_utc(date)
 
-            for hour in range(start, end + 1):
+            for hour in range(start_hour, end_hour + 1):
                 hour = str(hour)
-                image_url = get_image_url_from_api(api="https://svs.gsfc.nasa.gov/api/dialamoon", date=today, time=hour)
+                image_url = get_image_url_from_api(api="https://svs.gsfc.nasa.gov/api/dialamoon", date=date, time=hour)
                 filepath = get_filepath(
                     download_dir=os.path.abspath(os.path.join(os.environ.get("HOMEPATH"), "Downloads", "Moon Phases")),
-                    date=today,
+                    date=date,
                     time=hour
                 )
                 download_image(url=image_url, path=filepath)
@@ -69,6 +82,8 @@ if __name__ == "__main__":
             print("Please enter a value between 00 and 23.")
         except HoursError:
             print("The hour of the first Moon visualization image should be earlier then the last.")
+        except DateError:
+            print("The given date is not valid!")
         except KeyboardInterrupt:
             sys.exit("Program stopped by user.")
         except requests.exceptions.ConnectionError:
