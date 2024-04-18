b776fc0a3e441a4c8d7609e5fff8ffd28c901e3e
arkadiusz-l <104087320+arkadiusz-l@users.noreply.github.com>
2024-04-18 13:43:12 +0200

    Refactor

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import logging
+from typing import Tuple
 from datetime import datetime, timedelta, timezone
 import requests
 from tqdm import tqdm
@@ -85,6 +86,70 @@ def get_filepath(download_dir: str, date: str, hour: str) -> str:
     return filepath
 
 
+def get_date_from_user() -> str:
+    while True:
+        try:
+            date = input(
+                "Enter the date for which I should download images.\n"
+                "You can enter YYYY-MM-DD or 't' for today, 'tm' for tommorow.\n"
+                "You can also enter '+1' for tommorow, '+2' for the day after tommorow, etc: "
+            )
+            date = parse_user_date(date)
+        except ValueError:
+            print("\nThe date is not valid! Please enter a valid date.\n")
+            continue
+        return date
+
+
+def get_hours_from_user() -> Tuple[int, int]:
+    hour_value_error_message = "\nPlease enter a value between 0 and 23.\n"
+    while True:
+        try:
+            user_start_hour = get_hour_from_user(text="first")
+            user_end_hour = get_hour_from_user(text="last")
+            if user_start_hour > user_end_hour:
+                raise HoursOrderError
+        except ValueError:
+            print(hour_value_error_message)
+            continue
+        except HourValueError:
+            print(hour_value_error_message)
+            continue
+        except HoursOrderError:
+            print("\nThe hour of the first Moon visualization image should be earlier then the last one.\n")
+            continue
+        return user_start_hour, user_end_hour
+
+
+def download_images() -> int:
+    downloaded = 0
+    try:
+        for user_hour in range(user_start_hour, user_end_hour + 1):
+            user_hour = str(user_hour)
+            utc_date = convert_user_date_and_hour_to_utc(date=date, hour=user_hour)
+            url = get_image_url_from_api(api="https://svs.gsfc.nasa.gov/api/dialamoon", date=utc_date)
+            filepath = get_filepath(
+                download_dir=os.path.abspath(
+                    os.path.join(os.environ.get("HOMEPATH"), "Downloads", "Moon Visualizations")
+                ),
+                date=date,
+                hour=user_hour
+            )
+            download_image(url=url, path=filepath)
+            downloaded += 1
+        return downloaded
+    except DateOutOfRangeError:
+        print(f"The date and time must be between 2011-01-01 00:00 UTC and {datetime.now().year}-12-31 23:00 UTC.")
+    except requests.exceptions.SSLError:
+        sys.exit("SSL certificate verify failed!")
+    except requests.exceptions.ConnectionError:
+        sys.exit("API did not respond! Check API URL or network connection!")
+    except requests.exceptions.ChunkedEncodingError or urllib3.exceptions.ProtocolError:
+        sys.exit("Connection aborted! Check your network connection!")
+    finally:
+        print(f"{downloaded} files downloaded!")
+
+
 if __name__ == "__main__":
     logging_level = logging.INFO
     if len(sys.argv) > 1:
@@ -97,71 +162,14 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging_level)
     logging.getLogger("urllib3").setLevel(logging.WARNING)  # disable standard DEBUG logs from the 'requests' library
 
-    date = None
-    user_start_hour = None
-    user_end_hour = None
-    hour_value_error_message = "Please enter a value between 0 and 23."
-
     try:
-        while True:
-            try:
-                date = input(
-                    "Enter the date for which I should download images.\n"
-                    "You can enter YYYY-MM-DD or 't' for today, 'tm' for tommorow.\n"
-                    "You can also enter '+1' for tommorow, '+2' for the day after tommorow, etc: "
-                )
-                date = parse_user_date(date)
-            except ValueError:
-                print("The date is not valid! Please enter a valid date.")
-                continue
-            break
-
-        while True:
-            try:
-                user_start_hour = get_hour_from_user(text="first")
-                user_end_hour = get_hour_from_user(text="last")
-                if user_start_hour > user_end_hour:
-                    raise HoursOrderError
-            except ValueError:
-                print(hour_value_error_message)
-                continue
-            except HourValueError:
-                print(hour_value_error_message)
-                continue
-            except HoursOrderError:
-                print("The hour of the first Moon visualization image should be earlier then the last one.")
-                continue
-
-            choice = input(f"{user_end_hour - user_start_hour + 1} file(s) will be downloaded. Enter 'y' if continue: ")
-            if choice == "y":
-                downloaded = 0
-                try:
-                    for user_hour in range(user_start_hour, user_end_hour + 1):
-                        user_hour = str(user_hour)
-                        utc_date = convert_user_date_and_hour_to_utc(date=date, hour=user_hour)
-                        url = get_image_url_from_api(api="https://svs.gsfc.nasa.gov/api/dialamoon", date=utc_date)
-                        filepath = get_filepath(
-                            download_dir=os.path.abspath(
-                                os.path.join(os.environ.get("HOMEPATH"), "Downloads", "Moon Visualizations")
-                            ),
-                            date=date,
-                            hour=user_hour
-                        )
-                        download_image(url=url, path=filepath)
-                        downloaded += 1
-                        print("Done.")
-                except DateOutOfRangeError:
-                    print(f"The date and time must be between 2011-01-01 00:00 UTC and {datetime.now().year}-12-31 23:00 UTC.")
-                except requests.exceptions.SSLError:
-                    sys.exit("SSL certificate verify failed!")
-                except requests.exceptions.ConnectionError:
-                    sys.exit("API did not respond! Check API URL or network connection!")
-                except requests.exceptions.ChunkedEncodingError or urllib3.exceptions.ProtocolError:
-                    sys.exit("Connection aborted! Check your network connection!")
-                except Exception as error:
-                    sys.exit(error)
-                finally:
-                    print(f"{downloaded} files downloaded!")
-            break
+        date = get_date_from_user()
+        user_start_hour, user_end_hour = get_hours_from_user()
+        choice = input(f"{user_end_hour - user_start_hour + 1} file(s) will be downloaded. Enter 'y' if continue: ")
+        if choice == "y":
+            download_images()
+            print("Done.")
     except KeyboardInterrupt:
         sys.exit("The program has been stopped by user.")
+    except Exception as error:
+        sys.exit(error)
